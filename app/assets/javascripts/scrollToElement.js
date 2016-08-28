45f9bc286f62cb8cb2d2059951c26d7ab2d8c860
John Bernier <john.b.bernier@gmail.com>
2016-08-27 22:26:01 -0400

    correct page title

--- app/assets/javascripts/scrollToElement.js
+++ app/assets/javascripts/scrollToElement.js
@@ -1,10 +1,27 @@
 $(document).on('click', 'a[href^="#"]', function(e){
-	var target = $(this.getAttribute('href'));
-	if(target.length) {
-		e.preventDefault();
-		$('html, body').stop().animate({
-			scrollTop: target.offset().top
-		}, 1000);
-	}
+
+  var scroll = function(that,e) {
+    var target = $(that.getAttribute('href'));
+    if(target.length) {
+      e.preventDefault();
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top
+      }, 1000);
+    }
+  };
+
+  var changePageTitle = function(that) {
+    var target = $(that.getAttribute('href'));
+    if(target.length) {
+      var title = document.title;
+      var title = title.split("|")[0];
+      var pageSection = target.selector;
+      pageSection = pageSection.split("#")[1];
+      document.title = title +" | "+pageSection;
+    }
+  };
+
+  scroll(this,e);
+  changePageTitle(this);
 });
 
